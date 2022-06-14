7d249d1267b3f0a85b56272a999e1a660165c385
Zoltan Medve <debugmedve@gmail.com>
2022-06-14 14:53:30 +0200

    additional documentation

--- src/main/java/hu/pilopsoft/demo/service/RoomService.java
+++ src/main/java/hu/pilopsoft/demo/service/RoomService.java
@@ -11,6 +11,14 @@ public interface RoomService {
     /**
      * Calculates the total prices from the given number of free rooms and the potential customer's bids
      *
+     * Our hotel clients have two different categories of rooms: Premium and Economy. Our
+     * hotels want their customers to be satisfied: they will not book a customer willing to
+     * pay EUR 100 or more for the night into an Economy room. But they will book lower
+     * paying customers into Premium rooms if these rooms are empty and all Economy
+     * rooms are occupied with low paying customers. The highest paying customers below
+     * EUR 100 will get preference for the “upgrade”. Customers always only have one
+     * specific price they are willing to pay for the night.
+     *
      * @param request {@link RoomRequest}
      * @return {@link RoomResponse}
      */
